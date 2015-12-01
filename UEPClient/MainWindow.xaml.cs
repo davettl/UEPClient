ae8e3058d4e6a96d296127c6686d052b549cc3e2
Dave Tuttle <davettl@gmail.com>
2015-12-01 16:12:10 +0000

    Test first commit

---
+++ UEPClient/MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace UEPClient
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void menuServerUrlClick(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Current base URL is http://localhost:8080", "Base Url");
+        }
+
+        private void menuImportClick(object sender, RoutedEventArgs e)
+        {
+
+        }
+        private void menuExportClick(object sender, RoutedEventArgs e)
+        {
+
+        }
+        private void menuExitClick(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+    }
+}
